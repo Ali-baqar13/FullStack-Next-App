07aa2d85ce4e04ccd428d1d20d033bcedd4c440f
Ali Baqar <baqar2502@gmail.com>
2026-05-14 19:29:14 +0500

    first commit

---
+++ model/User.ts
@@ -0,0 +1,50 @@
+import mongoose, {Document,  Schema} from "mongoose";
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+
+}
+
+export interface User extends Document {
+    username: {type:string, required: true, unique: true, trim:true};
+    email: {type:string, required: true, unique: true, trim:true};
+    password: string;
+    isVerified: boolean;
+    messages: Message[];
+    verifyCode: string;
+    verifyCodeExpires: Date;
+    isAcceptingMessages: boolean;
+
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: { type: String, requirsed: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "Please use a valid email address."],
+  },
+  password: { type: String, required: [true, "Password is required"] },
+  verifyCode: { type: String, required: [true, "Verify code is required"] },
+  verifyCodeExpires: { type: Date, default: Date.now },
+  isVerified: { type: Boolean, default: false },
+  isAcceptingMessages: { type: Boolean, default: true },
+  messages: [MessageSchema]
+});
+
+
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+export default UserModel;
---
+++ schemas/signUpSchema.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+
+export const userNameValidation = z
+    .string()
+    .min(2, 'username must be at least 2 characters')
+    .max(20, 'username must be at most 20 characters')
+    .regex(/^[a-zA-Z0-9_]+$/, 'username can only contain letters, numbers, and underscores');
+
+export const signUpSchrma = z.object({
+    username: userNameValidation,
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z.string().min(6, 'password must be at least 6 characters').max(100, 'password must be at most 100 characters'),
+
+})
+        
